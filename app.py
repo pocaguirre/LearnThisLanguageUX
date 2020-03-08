365e6a1aa702591766d488717bb7f21325624bfa
Carlos Aguirre <caguirr4@jhu.edu>
2020-03-08 14:37:07 -0400

    checkpoint right before dashboard dev

--- app.py
+++ app.py
@@ -5,10 +5,23 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def landing_page():
     comment = get_comment(
         url="https://www.reddit.com/r/iPadPro/comments/fduvm7/apple_pencil_help/fjl0r10?utm_source=share&utm_medium=web2x")
     return render_template("index.html", comments=[comment])
+
+
+@app.route('/u/<user>')
+def user_feed_page(user):
+    comment = get_comment(
+        url="https://www.reddit.com/r/iPadPro/comments/fduvm7/apple_pencil_help/fjl0r10?utm_source=share&utm_medium=web2x")
+    return render_template("gen_feed.html", comments=[comment])
+
+@app.route('/u/<user>/myfeed')
+def user_my_feed(user):
+    comment = get_comment(
+        url="https://www.reddit.com/r/iPadPro/comments/fduvm7/apple_pencil_help/fjl0r10?utm_source=share&utm_medium=web2x")
+    return render_template("my_feed.html", comments=[comment])
 
 
 if __name__ == '__main__':
